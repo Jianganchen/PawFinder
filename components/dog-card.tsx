373129e12622da7453de16e4d11c2d8be6be43f9
Jianganchen <jianganchen@ufl.edu>
2025-03-06 00:13:57 -0500

    Fixed dog-card styling

--- components/dog-card.tsx
+++ components/dog-card.tsx
@@ -1,33 +1,35 @@
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Dog } from "@/lib/definitions";
+import { Heart } from "lucide-react";
 
 export function DogCard(dogProps: Dog) {
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>{dogProps.name}</CardTitle>
-        <CardDescription>age: {dogProps.age}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div>
+    <Card className="w-[300px] overflow-hidden p-0">
+      <CardContent className="grid p-0">
+        <div className="relative">
+          <Button
+            className="absolute z-10 top-1 right-1 rounded-full"
+            variant="secondary"
+          >
+            <Heart className="w-6 h-6 text-red-500" />
+          </Button>
           <img
             src={dogProps.img}
             alt={dogProps.name}
-            className="w-full h-[200px] object-cover rounded-lg"
+            className="w-full h-[200px] object-cover"
           />
         </div>
+        <div className="text-center p-4 space-y-2">
+          <h3 className="text-xl font-semibold text-gray-800">
+            {dogProps.name}
+          </h3>
+          <p className="text-sm text-gray-500">
+            {dogProps.breed} | Age: {dogProps.age}
+          </p>
+        </div>
       </CardContent>
-      <CardFooter className="flex justify-end">
-        <Button variant="outline">Adopt</Button>
-      </CardFooter>
+      <CardFooter className="flex justify-end"></CardFooter>
     </Card>
   );
 }
