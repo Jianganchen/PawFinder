799c365a4dc906aadc28ea054ff236b877159d6f
Jianganchen <jianganchen@ufl.edu>
2025-03-07 21:31:07 -0500

    Added favorites page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,24 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Separator } from "@radix-ui/react-separator";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </header>
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,8 @@
-import { AppSidebar } from "@/components/app-sidebar";
 import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { DogResults } from "@/components/dog-results";
 
-export default async function Home(props: {
+export default async function Dashboard(props: {
   searchParams?: Promise<{
     page?: string;
     breed?: string;
@@ -22,24 +17,13 @@ export default async function Home(props: {
   const currentZipcode = searchParams?.zipcode || undefined;
 
   return (
-    <div>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-          </header>
-          <div className="flex flex-1 flex-col gap-4 p-4">
-            <DogResults
-              currentPage={currentPage}
-              currentBreed={currentBreed}
-              currentSort={currentSort}
-              currentZipcode={currentZipcode}
-            />
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
+    <div className="flex flex-1 flex-col gap-4 p-4">
+      <DogResults
+        currentPage={currentPage}
+        currentBreed={currentBreed}
+        currentSort={currentSort}
+        currentZipcode={currentZipcode}
+      />
     </div>
   );
 }
